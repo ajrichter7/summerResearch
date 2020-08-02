acdc5c601d84704a9e32319c780fff89820829e3
Alex <ajrichter@reed.edu>
2020-08-01 17:35:26 -0700

    untrack

--- HyperGraph1.0/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.reed.hypergraph","l":"Bobject"},{"p":"edu.reed.hypergraph","l":"FileIO"},{"p":"edu.reed.hypergraph","l":"HyperAlgorithms"},{"p":"edu.reed.hypergraph","l":"HyperEdge"},{"p":"edu.reed.hypergraph","l":"HyperGraph"},{"p":"edu.reed.hypergraph","l":"HyperNode"}]
